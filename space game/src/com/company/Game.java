5579cbe35f751a9b1ddf53ee6eafab673061dc5b
Sherkhan Khanseiit <57889104+Sherhan187@users.noreply.github.com>
2020-06-12 21:51:51 +0600

    Add files via upload
    not finished project

---
+++ space game/src/com/company/Game.java
@@ -0,0 +1,141 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+
+public class Game extends Canvas implements Runnable {
+
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = WIDTH / 12*9;
+    public static final int SCALE = 2;
+    public  final String TITLE = "Space Game";
+
+    private boolean running = false;
+    private Thread thread;
+
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_BGR);
+   // private BufferedImage spriteSheet = null;
+
+    //temp
+    private BufferedImage player;
+
+   /** public void init() {
+        BufferedImageLoader loader = new BufferedImageLoader();
+        try {
+            spriteSheet = loader.loadImage("C:\\Users\\Шерхан\\Desktop\\space game\\res\\space_sheet.png");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        SpriteSheet ss = new SpriteSheet(spriteSheet);
+        player= ss.grabImage(1,1,32,32);
+    } **/
+
+
+
+    private synchronized void start(){
+        if (running)
+            return;
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    private synchronized void stop() {
+        if(!running)
+            return;
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+    }
+
+
+    public void run(){
+      //  init();
+        long lastTime = System.nanoTime();
+        final double amountOFTicks = 60.0;
+        double ns = 1000000000 / amountOFTicks;
+        double delta = 0;
+        int updates = 0;
+        int frames = 0;
+        long timer = System.currentTimeMillis();
+        
+        while (running){  //this will be game loop
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now;
+            if (delta >= 1){
+                tick();
+                updates++;delta--;
+            }
+            render();
+            frames++;
+            
+            if (System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                System.out.println(updates + " Ticks , Fps " + frames);
+                updates = 0;
+                frames = 0;
+            }
+        }
+        stop();
+    }
+    
+    private void tick(){
+        
+    }
+    
+    private void render(){
+    BufferStrategy bs = this.getBufferStrategy();
+
+    if (bs == null){
+        createBufferStrategy(3);
+        return;
+        }
+    Graphics g = bs.getDrawGraphics();
+    ///////////////////
+        g.drawImage(image, 0 ,0,getWidth(),getHeight(), this);
+
+        //g.drawImage(player, 100,100,this);
+        /////////////////
+    g.dispose();
+    bs.show();
+    }
+    
+    
+
+
+    // main *
+    public static void main(String[] args){
+        Game game = new Game();
+
+        game.setPreferredSize(new Dimension(WIDTH* SCALE, HEIGHT * SCALE));
+        game.setMaximumSize(new Dimension(WIDTH* SCALE, HEIGHT * SCALE));
+        game.setMinimumSize(new Dimension(WIDTH* SCALE, HEIGHT * SCALE));
+
+        JFrame frame = new JFrame(game.TITLE);
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable((false));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+        game.start();
+
+    }
+
+
+
+
+
+}
